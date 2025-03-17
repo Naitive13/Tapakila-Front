71d0b075f77d0723ee62cabd31e7a5c957aa4de1
Naitive13 <hei.titan.2@gmail.com>
2025-03-17 17:45:48 +0300

    feat: add simple login page
    the handler is just for testing for now

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,57 @@
+"use client";
+import { useRef } from "react";
+
 export default function Login() {
+  // const handler = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log({
+  //     username: usernameRef.current?.value,
+  //     password: passwordRef.current?.value,
+  //     email: emailRef.current?.value,
+  //     role: "standard",
+  //     creationDate: new Date().toISOString(),
+  //   });
+  // };
+
+  const handler = async (e: FormData) => {
+    console.log({
+      username: e.get("username"),
+      password: e.get("password"),
+      // email: e.get("email"),
+      // role: "standard",
+      // creationDate: new Date().toISOString(),
+    });
+  };
+
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div>
-      <h1>Login</h1>
-      <button>Login</button>
-    </div>
+    <>
+      <form action={handler}>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Username</legend>
+          <input
+            type="text"
+            className="input"
+            placeholder=""
+            name="username"
+            ref={usernameRef}
+          />
+        </fieldset>
+
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Password</legend>
+          <input
+            type="password"
+            className="input"
+            placeholder=""
+            name="password"
+            ref={passwordRef}
+          />
+        </fieldset>
+        <button>send</button>
+      </form>
+    </>
   );
 }
