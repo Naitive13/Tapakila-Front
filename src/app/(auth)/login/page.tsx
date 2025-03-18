fb98fb7f3329a6e2bbc43cb1c2129287ef665119
Naitive13 <hei.titan.2@gmail.com>
2025-03-18 17:23:11 +0300

    fix: button css

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,10 @@ import { loginAction } from "./actions";
 
 export default function Login() {
   return (
-    <>
+    <form
+      action={loginAction}
+      className="flex justify-between items-stretch py-5 px-5 w-8/12 rounded-md border xl:px-5 xl:max-w-7xl bg-base-100 drop-shadow-xl border-black/20"
+    >
       <div className="sm:w-[60%] lg:w-[50%] bg-cover bg-center items-center justify-center hidden md:flex ">
         <img
           src="https://www.vibrantacademy.com/user/img/login.png"
@@ -12,40 +15,38 @@ export default function Login() {
         />
       </div>
 
-      <form action={loginAction}>
-        <div className="flex flex-col justify-center py-5 mx-auto w-full md:p-10 md:py-0 lg:w-1/2">
-          <h1 className="text-2xl font-semibold text-center sm:text-3xl text-primary">
-            Welcome Back
-          </h1>
-          <div className="mt-5 w-full sm:mt-8">
-            <div className="flex flex-col gap-5 mx-auto w-full sm:max-w-md md:max-w-lg">
-              <input
-                type="text"
-                name="username"
-                placeholder="Enter Your Username"
-                className="w-full text-base-content input input-bordered input-primary placeholder:text-base-300"
-              />
-              <input
-                type="Password"
-                name="password"
-                placeholder="Enter Your Password"
-                className="w-full text-base-content input input-bordered input-primary placeholder:text-base-300"
-              />
-              <div className="flex flex-col gap-2 justify-between items-center md:flex-row md:gap-4">
-                <button
-                  type="submit"
-                  className="btn btn-active btn-primary btn-block max-w-6/12"
-                >
-                  Log In
-                </button>
-                <button className="btn btn-outline btn-primary btn-block max-w-6/12">
-                  Sign Up
-                </button>
-              </div>
+      <div className="flex flex-col justify-center py-5 mx-auto w-full md:p-10 md:py-0 lg:w-1/2">
+        <h1 className="text-2xl font-semibold text-center sm:text-3xl text-primary">
+          Welcome Back
+        </h1>
+        <div className="mt-5 w-full sm:mt-8">
+          <div className="flex flex-col gap-5 mx-auto w-full sm:max-w-md md:max-w-lg">
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Your Username"
+              className="py-5 w-full text-base-content input input-bordered input-primary placeholder:text-base-300"
+            />
+            <input
+              type="Password"
+              name="password"
+              placeholder="Enter Your Password"
+              className="w-full text-base-content input input-bordered input-primary placeholder:text-base-300"
+            />
+            <div className="flex flex-col gap-x-2 gap-y-2 justify-between items-center md:flex-row">
+              <button
+                type="submit"
+                className="w-6/12 btn btn-active shrink btn-primary md:max-w-12/12"
+              >
+                Log In
+              </button>
+              <button className="w-6/12 btn btn-outline shrink btn-primary md:max-w-12/12">
+                Sign Up
+              </button>
             </div>
           </div>
         </div>
-      </form>
-    </>
+      </div>
+    </form>
   );
 }
