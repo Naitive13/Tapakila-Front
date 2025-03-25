421f990fff06b192d1a9013f8f459356d50e5f6f
Naitive13 <hei.titan.2@gmail.com>
2025-03-25 09:26:59 +0300

    feat: replace loading text with loading component

--- src/app/(other)/user/page.tsx
+++ src/app/(other)/user/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Loading from "@/components/Loading";
 import UserProfileCard from "@/components/UserProfileCard";
 import { getData } from "@/lib/fetcher";
 import useSWR from "swr";
@@ -6,23 +7,19 @@ import useSWR from "swr";
 export default function UserPage() {
   const { data, error, isLoading } = useSWR(`api/user`, getData);
   if (isLoading) {
-    return (
-      <div>
-        <h1>Loading user data...</h1>
-      </div>
-    );
+    return <Loading />;
   }
   if (error) {
     return <div> Error loading user data</div>;
   }
 
   return (
-    <div className="flex gap-1 justify-center items-center py-5 px-5 min-w-full bg-base-200">
+    <>
       <div className="flex justify-between items-stretch py-5 px-5 w-3/12 rounded-md shadow-md xl:px-5 bg-base-100 item-center">
         <UserProfileCard info={data} />
       </div>
       <div className="flex justify-between items-stretch py-5 px-5 w-5/12 rounded-md xl:px-5 bg-base-200"></div>
       <div className="flex justify-between items-stretch py-5 px-5 w-4/12 rounded-md xl:px-5 bg-base-200"></div>
-    </div>
+    </>
   );
 }
