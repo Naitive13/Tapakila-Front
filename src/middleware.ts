f55265ce9c378382c11d35c61cbd66a278296253
Naitive13 <hei.titan.2@gmail.com>
2025-04-01 21:32:57 +0300

    feat: add reservation to middleware list

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  if (pathname.startsWith("/user")) {
-    const token = request.cookies.get("accessToken");
-    console.log(token);
-    if (!token) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  const token = request.cookies.get("accessToken");
+  console.log(token);
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/user/:path*"],
+  matcher: ["/user/:path*", "/event/:path/reservation"],
 };
