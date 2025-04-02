1bdbbf6c638f11daac61a055302ac5fd1803f004
Naitive13 <hei.titan.2@gmail.com>
2025-04-02 16:03:55 +0300

    feat: create a mock server for reservations

---
+++ src/app/api/reservation/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { BASE_URL } from "../constant";
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  if (data.eventId == "test") {
+    if (data.ticketAmount == "5") {
+      return NextResponse.json(
+        { error: "You have reached the maximum number of reservation" },
+        { status: 500 },
+      );
+    }
+    if (data.ticketType == "VIP") {
+      return NextResponse.json(
+        { error: "VIP ticket is not available" },
+        { status: 500 },
+      );
+    }
+    return NextResponse.json({ message: "reservation successful" });
+  }
+}
