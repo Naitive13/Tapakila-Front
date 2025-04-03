4f30a5d281f50147b3736971a23fa1315d73bd08
Naitive13 <hei.titan.2@gmail.com>
2025-04-03 18:05:54 +0300

    feat: update reservation design

--- src/app/(other)/event/[eventId]/reservation/page.tsx
+++ src/app/(other)/event/[eventId]/reservation/page.tsx
@@ -5,7 +5,7 @@ import { getData } from "@/lib/fetcher";
 import { use, useRef } from "react";
 import useSWR from "swr";
 import { submit } from "./action";
-import useReservationInfoStore from "@/lib/reservationStore";
+import ReservationForm from "@/components/ReservationForm";
 
 type Params = {
   params: Promise<{ eventId: string }>;
@@ -15,9 +15,6 @@ export default function ReservationPage({ params }: Params) {
   const { data, error, isLoading } = useSWR(`/api/event/${eventId}`, getData);
   const ref = useRef<HTMLFormElement>(null);
 
-  const { ticketType, ticketAmount, setTicketType, setTicketAmount } =
-    useReservationInfoStore();
-
   if (isLoading) {
     return <Loading />;
   }
@@ -26,79 +23,22 @@ export default function ReservationPage({ params }: Params) {
   }
   return (
     <>
-      <div className="flex overflow-hidden flex-col max-w-full h-full shadow-md md:flex-row w-dvw shrink max-h-fit md:max-h-[65dvh]">
-        <div className="flex overflow-hidden justify-center items-center min-w-5/12">
-          <img
-            src={data.eventPoster}
-            alt="poster"
-            className="self-center size-max"
-          />
-        </div>
-        <div className="flex flex-col p-4 min-w-8/12">
-          <h2 className="mb-2.5 text-3xl font-semibold md:text-4xl">
-            Make Your Reservation Now !
-          </h2>
-          <ReservationEvent data={data} />
-          <form
-            action={async (formData) => {
-              submit(formData);
-              ref.current?.reset();
-            }}
-            ref={ref}
-            onReset={() => {
-              setTicketAmount("1");
-              setTicketType("Regular");
-            }}
-          >
-            <fieldset className="my-4 fieldset">
-              <legend className="text-xl fieldset-legend">Ticket Type</legend>
-              <input
-                name="ticketType"
-                type="hidden"
-                defaultValue={ticketType}
-              />
-              <input
-                name="ticketAmount"
-                type="hidden"
-                defaultValue={ticketAmount}
-              />
-              <input name="eventId" type="hidden" defaultValue={eventId} />
-              <select
-                defaultValue="Regular"
-                className="w-2/5 opacity-60 select select-secondary"
-                onChange={(e) => setTicketType(e.target.value)}
-              >
-                <option>Regular</option>
-                <option>Bronze</option>
-                <option>Silver</option>
-                <option>Gold</option>
-                <option>VIP</option>
-              </select>
-            </fieldset>
-            <fieldset className="fieldset">
-              <legend className="text-xl fieldset-legend">Ticket Amount</legend>
-              <select
-                defaultValue="1"
-                className="w-2/5 opacity-60 select select-secondary"
-                onChange={(e) => setTicketAmount(e.target.value)}
-              >
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-            </fieldset>
-            <button className="mt-4 w-2/12 btn btn-soft btn-primary">
-              Confirm
-            </button>
-            <button
-              type="button"
-              className="mt-4 ml-16 w-2/12 btn btn-soft btn-error"
-            >
-              Cancel
-            </button>
-          </form>
+      <div className="flex justify-center items-center p-10 h-[90dvh]">
+        <div className="flex overflow-hidden flex-col justify-center items-center max-w-full rounded-lg shadow-md md:flex-row md:max-h-full bg-base-100 shrink max-h-fit">
+          <div className="flex overflow-hidden justify-center items-center min-w-5/12">
+            <img
+              src={data.eventPoster}
+              alt="poster"
+              className="self-center size-max"
+            />
+          </div>
+          <div className="flex flex-col p-10 min-w-7/12">
+            <h2 className="mb-2.5 text-3xl font-semibold md:text-4xl">
+              Make Your Reservation Now !
+            </h2>
+            <ReservationEvent data={data} />
+            <ReservationForm action={submit} ref={ref} eventId={eventId} />
+          </div>
         </div>
       </div>
     </>
