0d7dd97adeb53e176f22aa1f067a9dc397e8d1b8
Naitive13 <hei.titan.2@gmail.com>
2025-04-03 07:27:48 +0300

    fix: resize images to fit in correctly

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,13 +1,19 @@
 type CarouselCardProps = {
   id: number;
+  poster: string;
 };
 export default function CarouselCard(props: CarouselCardProps) {
   const previousId: number = props.id == 1 ? 4 : props.id - 1;
   const nextId: number = props.id == 4 ? 1 : props.id + 1;
   return (
     <>
-      <div id={`slide${props.id}`} className="relative w-full carousel-item">
-        <img src="https://placehold.co/1440x400" className="w-full" />
+      <div
+        id={`slide${props.id}`}
+        className="flex overflow-hidden relative w-full max-h-full carousel-item"
+      >
+        <div className="flex overflow-hidden justify-center items-center min-w-max">
+          <img src={props.poster} alt="poster" className="" />
+        </div>
         <div className="flex absolute right-5 left-5 top-1/2 justify-between transform -translate-y-1/2">
           <a href={`#slide${previousId}`} className="btn btn-circle">
             ❮
